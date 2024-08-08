4f8bdb9957d06feca0bd6cb8733156ec7eace57c
Logesh-Devops <55657365+Logesh-Devops@users.noreply.github.com>
2024-08-08 23:36:05 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,26 @@
+variable "docker_image" {
+  description = "Docker image to deploy"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "app" {
+  ami           = "ami-0ad21ae1d0696ad58"  # Replace with your desired AMI
+  instance_type = "t2.micro"
+  key_name      = "logesh"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              docker run -d --name myapp -p 80:80 ${var.docker_image}
+              EOF
+
+  tags = {
+    Name = "MyAppInstance"
+  }
+}
+
+output "instance_ip" {
+  value = aws_instance.app.public_ip
+}
