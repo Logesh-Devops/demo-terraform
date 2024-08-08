e904357a68271301dddb733b3e6ac547ad3700d4
Logesh-Devops <55657365+Logesh-Devops@users.noreply.github.com>
2024-08-09 00:22:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,8 +30,8 @@ resource "aws_instance" "app" {
               sudo systemctl enable docker
               # Remove existing container if it exists
               if [ $(sudo docker ps -aq -f name=myapp) ]; then
-                  sudo docker stop myapp
-                  sudo docker rm myapp
+                  sudo docker stop myapp --force
+                  sudo docker rm myapp --force
               fi
               # Run the Docker container
               sudo docker run -d --name myapp -p 5000:5000 ${var.docker_image}
