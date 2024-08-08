dadb564361d9519bcda47c83390ad413005b0a90
Logesh-Devops <55657365+Logesh-Devops@users.noreply.github.com>
2024-08-09 00:01:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,35 @@
-variable "docker_image" {
-  description = "Docker image to deploy"
-}
-
 provider "aws" {
   region = "ap-south-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "demo-vishal-docker-setup"
+    key    = "terraform/state"
+    region = "ap-south-1"
+  }
+}
+
+variable "docker_image" {
+  description = "Docker image to deploy"
+}
+
 resource "aws_instance" "app" {
-  ami           = "ami-0ad21ae1d0696ad58"  # Replace with your desired AMI
+  ami           = "ami-0ad21ae1d0696ad58"  # Ubuntu AMI in ap-south-1 region
   instance_type = "t2.micro"
   key_name      = "logesh"
 
   user_data = <<-EOF
               #!/bin/bash
-              docker run -d --name myapp -p 80:80 ${var.docker_image}
+              # Update the package list
+              sudo apt-get update -y
+              # Install Docker
+              sudo apt-get install -y docker.io
+              # Start Docker service
+              sudo systemctl start docker
+              sudo systemctl enable docker
+              # Run the Docker container
+              sudo docker run -d --name myapp -p 80:80 ${var.docker_image}
               EOF
 
   tags = {
